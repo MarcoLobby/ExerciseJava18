c7f601ce5419d5a1190dfd2bb1ca491822e22d38
Marco Lo Bello <xSalamander@outlook.it>
2022-12-12 20:30:39 +0100

    Initial Commit

---
+++ src/PrintYourNameAndSurname.java
@@ -0,0 +1,7 @@
+public class PrintYourNameAndSurname {
+    public static void main(String[] args) {
+        String name = "Marco ", surname = "Lo Bello";
+        System.out.print(name);
+        System.out.print(surname);
+    }
+}
